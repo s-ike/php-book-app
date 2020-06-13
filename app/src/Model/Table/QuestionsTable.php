0893d910c5cda6a32d7a742d11434c89d33b523d
s-ike <shunt_i@yahoo.co.jp>
2020-06-13 19:57:03 +0900

    Add: バリデーションルール

--- app/src/Model/Table/QuestionsTable.php
+++ app/src/Model/Table/QuestionsTable.php
@@ -3,6 +3,7 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\Validation\Validator;
 
 /**
  * Class QuestionsTable
@@ -44,5 +45,26 @@ class QuestionsTable extends Table
             ->enableAutoFields(true);
 
         return $query;
+    }
+
+    /**
+     * バリデーションルールの定義
+     *
+     * @param Validator $validator バリデーションインスタンス
+     * @return Validator バリデーションインスタンス
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->nonNegativeInteger('id', 'IDが不正です')
+            ->allowEmpty('id', 'IDが不正です');
+
+        $validator
+            ->scalar('body', '質問内容が不正です')
+            ->requirePresence('body', 'create', '質問内容が不正です')
+            ->notEmpty('body', '質問内容は必ず入力してください')
+            ->maxLength('body', 140, '質問内容は140文字以内で入力してください');
+
+        return $validator;
     }
 }
